495b0feca8098c0c8c1f8ac85961c67b2e42eb37
Jake Ratcliffe <JakeR1342@gmail.com>
2024-12-08 18:24:04 +0000

    Moved part 1 solution from part 2 file to part 1 file

--- 2024/day-03/src/bin/part-01.rs
+++ 2024/day-03/src/bin/part-01.rs
@@ -1,13 +1,24 @@
 use std::fs;
 
+use regex::Regex;
+
 fn main() {
-    println!("day-03");
+    let input = fs::read_to_string("./data/day-03.txt").expect("Unable to read file");
+    let result = part_1(&input);
+
+    println!("{}", result);
 }
 
 fn part_1(input: &str) -> i32 {
-    println!("Input: \"{}\"", input);
+    let re = Regex::new(r#"mul\((?P<first>[\d]{1,3}),(?P<second>[\d]{1,3})\)"#).unwrap();
 
-    return 161;
+    let mut result: i32 = 0;
+    for (_, [first, second]) in re.captures_iter(input).map(|c| c.extract()) {
+        let mul = first.parse::<i32>().unwrap() * second.parse::<i32>().unwrap();
+        result += mul;
+    }
+
+    return result;
 }
 
 #[cfg(test)]
@@ -16,10 +27,13 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        let input = r#"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"#;
-        let expected: i32 = 161;
-        let actual: i32 = part_1(&input);
+        let input = r#"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"#;
 
-        assert_eq!(actual, expected);
+        assert_eq!(part_1(input), 0);
     }
 }
